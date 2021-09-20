336252c63277dd7d5a24492df8653547248552d4
trisyabana22 <cobainaja1111@gmail.com>
2021-09-20 13:35:35 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae architecto, nisi veritatis optio incidunt, porro consequuntur pariatur eius eum similique maiores corporis eligendi odit id dicta ducimus culpa animi numquam!</p>
 </body>
 </html>
